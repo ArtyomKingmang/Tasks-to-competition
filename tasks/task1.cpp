ae5dfd89dd53ad461e6d253de5c675fab1c96bac
Kingmang <126554294+ArtyomKingmang@users.noreply.github.com>
2023-10-05 20:13:33 +0300

    init 2 tasks

---
+++ tasks/task1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    ofstream fout("OUTPUT.txt");
+    for (int i = 1000; i <= 9999; i++) { 
+        if (i % 7 == 0) {
+            int sum = 0;
+            int num = i;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            if (sum % 7 == 0) {
+                fout << i << endl;
+            }
+        }
+    }
+    fout.close();
+    return 0;
+}
---
+++ tasks/task2.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int sum(int num) {
+    int sum = 0;
+    while (num > 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+bool compare(int a, int b) {
+    int sumA = sum(a);
+    int sumB = sum(b);
+    if (sumA == sumB) {
+        return a < b;
+    }
+    return sumA < sumB;
+}
+
+int main() {
+    ifstream fin("INPUT.TXT");
+    ofstream fout("OUTPUT.TXT");
+
+    int n;
+    fin >> n;
+
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        fin >> nums[i];
+    }
+
+    sort(nums.begin(), nums.end(), compare);
+
+    for (int i = 0; i < n; i++) {
+        fout << nums[i] << " ";
+    }
+    fout << endl;
+
+    fin.close();
+    fout.close();
+
+    return 0;
+}
